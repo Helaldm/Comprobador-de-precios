4676cee5b1f3674f632c710eff32cf40c346bcc1
Helaldm <helenmillatruz17@gmail.com>
2025-06-14 21:37:25 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,34 @@
-const chaiHttp = require('chai-http');
 const chai = require('chai');
-const assert = chai.assert;
+const chaiHttp = require('chai-http');
 const server = require('../server');
-
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+describe('Stock Price API', () => {
+    it('should get the price of a stock', (done) => {
+        chai.request(server)
+            .get('/api/stock-prices?stock=GOOG')
+            .end((err, res) => {
+                chai.expect(res).to.have.status(200);
+                chai.expect(res.body).to.have.property('stockData');
+                chai.expect(res.body.stockData).to.be.an('array').that.is.not.empty;
+                chai.expect(res.body.stockData[0]).to.have.property('stock').equal('GOOG');
+                chai.expect(res.body.stockData[0]).to.have.property('price');
+                done();
+            });
+    });
 
+    it('should get the price of a stock and handle likes', (done) => {
+        chai.request(server)
+            .get('/api/stock-prices?stock=GOOG&like=true')
+            .end((err, res) => {
+                chai.expect(res).to.have.status(200);
+                chai.expect(res.body).to.have.property('stockData');
+                chai.expect(res.body.stockData).to.be.an('array').that.is.not.empty;
+                chai.expect(res.body.stockData[0]).to.have.property('stock').equal('GOOG');
+                chai.expect(res.body.stockData[0]).to.have.property('likes');
+                done();
+            });
+    });
+
+    // Additional tests can go here...
 });
